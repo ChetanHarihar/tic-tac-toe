89ba6bc382f8669cbd8461e82caec4a5179fd8b8
ChetanHarihar <chetanhs11@gmail.com>
2023-10-06 12:30:59 +0530

    created game file

---
+++ game.py
@@ -0,0 +1,16 @@
+board = ['', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ']
+
+
+def display_board():
+    print(f'   |   |   ')
+    print(f' {board[7]} | {board[8]} | {board[9]} ')
+    print(f'   |   |   ')
+    print(f'-----------')
+    print(f'   |   |   ')
+    print(f' {board[4]} | {board[5]} | {board[6]} ')
+    print(f'   |   |   ')
+    print(f'-----------')
+    print(f'   |   |   ')
+    print(f' {board[1]} | {board[2]} | {board[3]} ')
+    print(f'   |   |   ')
+
